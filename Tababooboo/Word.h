688bd10a0e15533901bb59c80938eed52bd16cfd
Jackson Owens <jackson_owens@brown.edu>
2013-12-24 16:23:31 -0500

    Word class started

---
+++ Tababooboo/Word.h
@@ -0,0 +1,17 @@
+//
+//  Word.h
+//  Tababooboo
+//
+//  Created by Jackson Owens on 12/24/13.
+//  Copyright (c) 2013 Tababooboo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Word : NSObject
+
+@property (readonly) int id;
+
+@property (readonly) NSString *word;
+
+@end
