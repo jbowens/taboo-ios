c12cc40052abc009d350cc47f78e931d9579ee75
Flora Jin <florajin14@gmail.com>
2013-12-27 18:51:15 -0500

    idk what i'm doing but i added a home view controller

---
+++ Tababooboo/HomeViewController.h
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.h
+//  Tababooboo
+//
+//  Created by Flora Jin on 12/27/13.
+//  Copyright (c) 2013 Tababooboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@property IBOutlet UIButton *playButton;
+@property IBOutlet UIButton *rulesButton;
+
+void playAction();
+
+@end
