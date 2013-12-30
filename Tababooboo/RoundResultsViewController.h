22c9f048fe63c93e9d1ee1f1ed6909eb22aa5f97
Flora Jin <florajin14@gmail.com>
2013-12-29 19:43:34 -0500

    added stencil for RoundResultsViewController

---
+++ Tababooboo/RoundResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoundResultsViewController.h
+//  Tababooboo
+//
+//  Created by Flora on 12/29/13.
+//  Copyright (c) 2013 Tababooboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundResultsViewController : UIViewController
+
+@end
---
+++ Tababooboo/RoundResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  RoundResultsViewController.m
+//  Tababooboo
+//
+//  Created by Flora on 12/29/13.
+//  Copyright (c) 2013 Tababooboo. All rights reserved.
+//
+
+#import "RoundResultsViewController.h"
+
+@interface RoundResultsViewController ()
+
+@end
+
+@implementation RoundResultsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
