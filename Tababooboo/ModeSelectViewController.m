b7d16d721e8205fe0b01cba93005cbcba4030004
Flora Jin <florajin14@gmail.com>
2013-12-31 13:15:54 -0500

    started mode selection controller

--- Tababooboo/ModeSelectViewController.m
+++ Tababooboo/ModeSelectViewController.m
@@ -7,8 +7,29 @@
 //
 
 #import "ModeSelectViewController.h"
+#import "Constants.h"
+
+@interface OptionButton : UIButton
+@property bool teamMode;
+@end
+
+@implementation OptionButton
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+@end
 
 @interface ModeSelectViewController ()
+
+@property OptionButton *practiceButton;
+@property OptionButton *teamButton;
 
 @end
 
@@ -28,6 +49,56 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.view.backgroundColor = PrimaryBackgroundColor;
+    [self addOptionButtons];
+}
+
+- (OptionButton *) createOption
+{
+    OptionButton *optionButton = [OptionButton buttonWithType:UIButtonTypeRoundedRect];
+    optionButton.translatesAutoresizingMaskIntoConstraints = NO;
+    optionButton.backgroundColor = PrimaryButtonBackgroundColor;
+    [optionButton setTitleColor:PrimaryHeaderColor forState:UIControlStateNormal];
+    optionButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    optionButton.titleLabel.font = [UIFont systemFontOfSize:InfiniteFontSize];
+    optionButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    optionButton.titleLabel.minimumScaleFactor = 0.1;
+    optionButton.titleLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+    [optionButton addTarget:self action:@selector(setGameMode:) forControlEvents:UIControlEventTouchUpInside];
+    
+    return optionButton;
+}
+
+- (void)addOptionButtons
+{
+    UIView *optionsView = [[UIView alloc] init];
+    optionsView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    // setting up option 1 button
+    self.practiceButton = [self createOption];
+    [self.practiceButton setTitle:@"Practice" forState:UIControlStateNormal];
+    self.practiceButton.teamMode = false;
+    [optionsView addSubview:self.practiceButton];
+    
+    // setting up option 2 button
+    self.teamButton = [self createOption];
+    [self.teamButton setTitle:@"Make Teams" forState:UIControlStateNormal];
+    self.teamButton.teamMode = true;
+    [optionsView addSubview:self.teamButton];
+    
+    [self.view addSubview:optionsView];
+    
+    /** NSDictionary *viewDict = @{
+                               @"optionsView": optionsView,
+                               @"prac": self.practiceButton,
+                               @"team": self.teamButton,
+                               };
+    */
+}
+
+- (void)setGameMode:(id)sender
+{
+    
 }
 
 - (void)didReceiveMemoryWarning
