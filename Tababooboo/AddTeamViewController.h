60dcff0e2a61d0876128ba257681ee73ed7a7152
Flora Jin <florajin14@gmail.com>
2014-01-01 15:31:18 -0500

    literally just added stencil for addteamviewcontroller

---
+++ Tababooboo/AddTeamViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTeamViewController.h
+//  Tababooboo
+//
+//  Created by Flora on 1/1/14.
+//  Copyright (c) 2014 Tababooboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTeamViewController : UIViewController
+
+@end
---
+++ Tababooboo/AddTeamViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddTeamViewController.m
+//  Tababooboo
+//
+//  Created by Flora on 1/1/14.
+//  Copyright (c) 2014 Tababooboo. All rights reserved.
+//
+
+#import "AddTeamViewController.h"
+
+@interface AddTeamViewController ()
+
+@end
+
+@implementation AddTeamViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
